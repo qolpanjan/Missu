a858b3274544b64bc2c66327c78b59757b3b8761
15365185687@qq.com <15365185687@qq.com>
2017-03-16 22:21:18 +0800

    所有界面逻辑已完善，进行数据之前的commit

--- app/src/main/java/com/missu/Activitys/LoginActivity.java
+++ app/src/main/java/com/missu/Activitys/LoginActivity.java
@@ -25,6 +25,14 @@ public class LoginActivity extends AppCompatActivity {
         login_user = (EditText)findViewById(R.id.et_login_user);
         login_password = (EditText)findViewById(R.id.et_login_password);
         login_go = (Button)findViewById(R.id.btn_login_login);
+        login_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         login_sign = (Button)findViewById(R.id.btn_login_sign);
         login_sign.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/missu/Activitys/WelcomeActivity.java
+++ app/src/main/java/com/missu/Activitys/WelcomeActivity.java
@@ -1,21 +1,34 @@
 package com.missu.Activitys;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
 import com.missu.R;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-          requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_welcome);
+        mhandler.sendEmptyMessageDelayed(1,2*1000);
+
 
 
     }
+
+    Handler mhandler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            Intent intent = new Intent(WelcomeActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
